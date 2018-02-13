497602915a706b0368a95ed8f3dc5206283dfdb2
berylliu <q_liu20@fanshaweonline.ca>
2018-02-13 12:05:44 +0800

    Qing's chatapp

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# chatApp
+## Node/socket.io Chat App
+Design a dark theme with randomized color for messages
+Using socket.io
+Using Grunt to compile and minify my production files 
--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-const express = require('express');//include this, kust like a php include
+// initialize our app and tell it to use some plugins from the modules folder
+const express = require('express');
 const app = express();
+const io = require('socket.io')();
 
-app.get('/', (req, res)=> {
-  res.sendFile(__dirname + '/index.html');
+// some config stuff
+const PORT = process.env.port || 3000;
+
+// tell our app to serve static files from the public folder
+app.use(express.static('public'));
+
+app.use(require('./routes/index'));
+app.use(require('./routes/contact'));
+app.use(require('./routes/users'));
+
+// tell the app to be served up at this port (same as WAMP or MAMP, just a different port)
+const server = app.listen(3000, function() {
+	console.log('listening on localhost:3000');
 });
 
-app.get('/contact', (req, res)=> {
-  res.sendFile(__dirname + '/contact.html');
-});
+io.attach(server);
 
-app.get('/portfolio', (req, res)=> {
-  res.sendFile(__dirname + '/portfolio.html');
-});
+// plug in socket.io
+io.on('connection', function(socket) {
+	console.log('a user has connected');
+	io.emit('chat message', { for: 'everyone', message: `${socket.id} is here to par TAY!!` });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+	// listen for a message, and then send it where it needs to go
+	socket.on('chat message', function(msg) {
+		console.log('message: ', msg);
+
+		// send a message event to all clients
+		io.emit('chat message', { for: 'everyone', message: msg });
+	});
+
+	// listen for disconnet
+	socket.on('disconnect', function() {
+		console.log('a user disconnected');
+		msg = `${socket.id} has left the building!`;
+		io.emit('disconnect message', msg);
+	});
 });
